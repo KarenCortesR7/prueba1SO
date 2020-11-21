4aa2fea1f78194d379da3c340b7e7c8bf3fb0cdf
krincon <krincon@localhost>
2020-11-21 00:20:40 -0500

    V.1.0 conexion basica y funcional, d euna interfaz y coneccion a base de datos.

---
+++ src/java/sessionbeanpk/CustomersFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sessionbeanpk;
+
+import entidades.Customers;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author krincon
+ */
+@Stateless
+public class CustomersFacade extends AbstractFacade<Customers> {
+
+    @PersistenceContext(unitName = "PracticaWeb1PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public CustomersFacade() {
+        super(Customers.class);
+    }
+    
+}
